cdb670f3dd0e73309e9beb5048429abe7d977c6b
Alishermale <alishermale@yandex.ru>
2022-08-07 15:09:04 +0300

    update __init_.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,3 +2,7 @@ from aiogram import Bot, Dispatcher, types
 from dotenv import load_dotenv
 import os
 from aiogram.utils import executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Command
+from states import Summ
